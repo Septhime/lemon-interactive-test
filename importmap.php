dd99fc7f0bec4f05ed71d97807641fd6b73392dd
Timothee Varin <timothee.varin.etu@univ-lille.fr>
2025-11-24 02:11:28 +0100

    Retrait de Stimulus-bundle & ux-turbo

--- importmap.php
+++ importmap.php
@@ -19,11 +19,8 @@ return [
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
-    '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
-    ],
     '@hotwired/turbo' => [
-        'version' => '7.3.0',
+        'version' => '8.0.20',
     ],
     'bootstrap' => [
         'version' => '5.3.8',
